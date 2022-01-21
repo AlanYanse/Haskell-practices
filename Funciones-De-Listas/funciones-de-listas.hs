3ad2491ef7405ca3e036e0881c020141cd983ba0
Alan Yanse <yansealan@gmail.com>
2022-01-21 15:32:32 -0300

    add new lesson

---
+++ Funciones-De-Listas/funciones-de-listas.hs
@@ -0,0 +1,134 @@
+
+-- length function
+
+{-
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+
+main = print(length lista)
+
+
+-- Output
+-- 7
+
+-}
+
+
+-- head function
+
+{-
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+
+main = print(head lista)
+
+
+-- Output
+-- 1
+
+-}
+
+
+-- tail function
+
+{-
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+
+main = print(tail lista)
+
+-- Output
+-- [2,3,4,5,6,7]
+
+-}
+
+-- last function
+
+{-
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+
+main = print(last lista)
+
+-- Output
+-- 7
+
+-}
+
+-- init function
+
+{-
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+
+main = print(init lista)
+
+-}
+
+-- Output
+-- [1,2,3,4,5,6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
