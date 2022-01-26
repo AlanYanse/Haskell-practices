e14e8c51a1b321d6bb3b0a679de168ac557c7047
Alan Yanse <yansealan@gmail.com>
2022-01-25 21:29:33 -0300

    update repository

--- Listas/listas.hs
+++ Listas/listas.hs
@@ -1,10 +1,157 @@
 
 --miLista = [2,3,4]
 
+{-
+
 listaDoble = [[0,1], [2,3]] 
 
 main=print (listaDoble !!0 !!1)
 
+-}
+
 -- output
 -- 1
+
+{-
+
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (reverse lista1)
+
+-}
+
+-- output
+-- [7,6,5,4,3,2,1]
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (take 3 lista1)
+
+-}
+
+-- output
+-- [1,2,3]
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (drop 2 lista1)
+
+-}
+
+-- output
+--[3,4,5,6,7]
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (minimum lista1)
+
+-}
+
+-- output
+--1
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (maximum lista1)
+
+-}
+
+-- output
+--7
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (sum lista1)
+
+-}
+
+-- output
+--28
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (product lista1)
+
+-}
+
+-- output
+--5040
+
+
+{-
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+
+main = print (4 `elem` lista1)
+
+-}
+
+-- output
+--true
+
+{-
+
+
+listaDeLista = ["ab", "cd"]
+
+main = print ('c' `elem` listaDeLista!!1)
+
+-}
+
+-- output
+--true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
