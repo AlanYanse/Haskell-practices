198a9bf3161bc696f6bc8ac70dc260292bacd907
Alan Yanse <yansealan@gmail.com>
2022-01-30 16:30:04 -0300

    update repository

---
+++ Listas-Infinitas/funciones-listas-infinitas.hs
@@ -0,0 +1,52 @@
+
+
+
+{-
+
+num = 5
+
+
+miLista = replicate 10 num
+
+main = print miLista
+
+-}
+
+-- Output
+-- [5,5,5,5,5,5,5,5,5,5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
